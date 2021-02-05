05f5717bad6bea7095e72f2134d125fee179f381
LulinhaOliveira <43713408+LulinhaOliveira@users.noreply.github.com>
2021-02-05 20:13:41 -0300

    Luigi Boss Movimentação

---
+++ Assets/LuigiBoss.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuigiBoss : MonoBehaviour
+{
+    public int lifePoints = 100;
+    private float velocity = 1.5f;
+    public float velocityBonus;
+    public Transform player;
+    public Animator luigiController;
+    public bool right_left = false;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.position.x <= -113 && player.position.y <= -2.88)
+        {
+
+            velocityBonus = 2.3f;
+            //Luigi Modo Ataque
+            luigiController.SetBool("Attack?", true);
+        }
+        else
+        {
+            velocityBonus = 1;
+            //Luigi Modo Passivo
+            luigiController.SetBool("Attack?", false);
+
+        }
+
+        if (transform.position.x <= -125.8 || transform.position.x >= -113 )
+        {
+            velocity = -(velocity); 
+            transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + 180, 0);
+  
+        }
+            transform.position = new Vector3(transform.position.x + ((velocity * velocityBonus) * Time.deltaTime), transform.position.y, transform.position.z);
+
+    }
+}
--- Assets/Scripts/CharacterDamage.cs
+++ Assets/Scripts/CharacterDamage.cs
@@ -25,4 +25,6 @@ public class CharacterDamage : MonoBehaviour
     {
         destroyEnemy(collision.collider);
     }
+
+
 }
